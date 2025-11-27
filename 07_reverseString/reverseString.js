238d2e50ba891cf652186556706488acb69c9538
Arcanedarkness <32292250+Arcanedarkness@users.noreply.github.com>
2025-11-27 11:25:51 +0100

    Add loop copies a string from back to front

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(reverseThis) {
+    let reversed = "";
 
+    for (let i = 0; i < reverseThis.length; i++) {
+        let charToCopy = reverseThis.length-(i+1);
+        reversed += reverseThis[charToCopy];  
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
