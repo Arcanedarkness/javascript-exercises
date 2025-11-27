1c9176697a48c1b4504f2a0238c3875b3baa3922
Arcanedarkness <32292250+Arcanedarkness@users.noreply.github.com>
2025-11-27 11:01:38 +0100

    Add body to function with error escape

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeatedString = "";
+    if (number > 0) {
+        for (let index = 0; index < number; index++) {
+             repeatedString += string;
+        }
+    } else {
+        return "ERROR";
+    }
+    return repeatedString;
+    
 };
 
 // Do not edit below this line
