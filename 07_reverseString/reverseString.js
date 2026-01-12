249a03c7c565acfe3ba1cf15ddddc3fd3452659b
Arcanedarkness <32292250+Arcanedarkness@users.noreply.github.com>
2026-01-12 09:52:19 +0100

    Change method of reversal to array methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,12 +1,18 @@
+// const reverseString = function(reverseThis) {
+//     let reversed = "";
+
+//     for (let i = 0; i < reverseThis.length; i++) {
+//         let charToCopy = reverseThis.length-(i+1);
+//         reversed += reverseThis[charToCopy];  
+//     }
+
+//     return reversed;
+// };
+
 const reverseString = function(reverseThis) {
-    let reversed = "";
+    
+    return reverseThis.split("").reverse().join("");
 
-    for (let i = 0; i < reverseThis.length; i++) {
-        let charToCopy = reverseThis.length-(i+1);
-        reversed += reverseThis[charToCopy];  
-    }
-
-    return reversed;
 };
 
 // Do not edit below this line
