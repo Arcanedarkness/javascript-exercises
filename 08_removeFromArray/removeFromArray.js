3b13f563258c73683ba9ef2006fbd18188c7f87d
Arcanedarkness <32292250+Arcanedarkness@users.noreply.github.com>
2026-01-12 10:09:00 +0100

    Add basic filter logic to removeFromArray method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, filterWord) {
+
+    return array.filter((word) => word != filterWord);
+
 };
 
 // Do not edit below this line
